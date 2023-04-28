80f2bf13e0b760605b78ae89b71d38585b82eb86
Abhishek Kumar <113116498+abhiraj-ku@users.noreply.github.com>
2023-04-28 22:13:05 +0530

    Chat gpt prompt with improved UI

--- chat gpt prompt/script.js
+++ chat gpt prompt/script.js
@@ -1,8 +1,8 @@
 const prompt = document.getElementById("prompt");
 const result = document.getElementById("result");
 
-// const pp = prompt.value;
-// console.log(pp);
+const pp = prompt.value;
+console.log(pp);
 
 function callGptApi() {
   const pp = prompt.value;
@@ -10,7 +10,7 @@ function callGptApi() {
     method: "POST",
     headers: {
       "content-type": "application/json",
-      "X-RapidAPI-Key": "16be04b17cmshd33d24d6d60cbf5p1f775ajsn9518885a434f",
+      "X-RapidAPI-Key": "API_KEY_HERE",
       "X-RapidAPI-Host": "openai80.p.rapidapi.com",
     },
     // body: '{"model":"gpt-3.5-turbo","messages":[{"role":"user","content":"${pp}"}]}',
@@ -27,3 +27,72 @@ function callGptApi() {
     prompt.innerText = "hehe ";
   }
 }
+
+const API_KEY = "API_KEY_HERE";
+
+const outputElem = document.getElementById("output");
+
+async function getMessage() {
+  console.log("clicked");
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: inputQuery.value }],
+      max_token: 100,
+    }),
+  };
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      options
+    );
+    const data = await response.json();
+    console.log(data);
+    outputElem.textContent = data.choices[0].messages.content;
+    if (data.choices[0].messages.content) {
+      const pElem = document.createElement("p");
+      pElem.textContent = inputQuery.value;
+      historyDiv.append(pElem);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+submitButton.addEventListener("click", getMessage);
+
+const sideBar = document.querySelector(".side-bar");
+const hambur = document.querySelector(".hamburger");
+const hamburTimes = document.querySelector(".hamburger-times");
+const inputQuery = document.querySelector(".input");
+const historyDiv = document.querySelector(".history");
+const submitButton = document.getElementById("submit");
+const buttonNew = document.querySelector("button");
+
+hambur.addEventListener("click", () => {
+  sideBar.classList.toggle("active");
+  // console.log("hello");
+});
+hamburTimes.addEventListener("click", () => {
+  sideBar.classList.remove("active");
+  // console.log("hello");
+});
+
+submitButton.addEventListener("click", () => {
+  if (inputQuery) {
+    const pElem = document.createElement("p");
+    pElem.textContent = inputQuery.value;
+    historyDiv.append(pElem);
+  }
+});
+buttonNew.addEventListener("click", () => {
+  historyDiv.innerHTML = "";
+  if (inputQuery) {
+    inputQuery.value = "";
+  }
+});
